a02d70ee9f87968c030c491a32273e527c0d3ca8
mecaneer23 <mecaneer23@gmail.com>
2023-01-14 13:02:12 -0600

    add calibration and dependencies

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Calibration/PIDTuner.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Calibration;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class PIDTuner {
+    public static double kP = 0;
+    public static double kI = 0;
+    public static double kD = 0;
+    public static double kF = 0;
+    public static double kPosP = 0;
+}

c80fa07bc46416e6eb7e939db2f6871901ed26a3
mecaneer23 <mecaneer23@gmail.com>
2023-01-14 15:49:22 -0600

    rename roadrunner files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/trajectorysequence/TrajectorySequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/trajectorysequence/TrajectorySequence.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.trajectorysequence;
+package org.firstinspires.ftc.teamcode.RoadRunner.trajectorysequence;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/trajectorysequence/TrajectorySequenceRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/trajectorysequence/TrajectorySequenceRunner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.trajectorysequence;
+package org.firstinspires.ftc.teamcode.RoadRunner.trajectorysequence;
 
 import androidx.annotation.Nullable;
 

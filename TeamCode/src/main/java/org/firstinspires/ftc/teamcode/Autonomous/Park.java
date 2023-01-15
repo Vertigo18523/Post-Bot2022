fc72b4de79bc52b3b73d2c0eb0747dd0a599960a
mecaneer23 <mecaneer23@gmail.com>
2023-01-15 01:57:28 -0600

    WOOOOOOO odo park auto is done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Park.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Park.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
@@ -7,12 +10,13 @@ import org.firstinspires.ftc.teamcode.Base.BaseOpMode;
 import org.firstinspires.ftc.teamcode.Base.Robot;
 import org.firstinspires.ftc.teamcode.Bots.PostBot;
 import org.firstinspires.ftc.teamcode.Components.Camera;
-import org.firstinspires.ftc.teamcode.Pipelines.SleeveDetection;
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.RRMecanum;
 
 @Autonomous
-@Disabled
 public class Park extends BaseOpMode {
     public PostBot robot;
+    public RRMecanum drive;
+    public Trajectory left, right, center;
 
     Camera.ParkingPosition parkingPosition;
 
@@ -29,6 +33,20 @@ public class Park extends BaseOpMode {
 
     @Override
     public void onInit() throws InterruptedException {
+        drive = new RRMecanum(hardwareMap);
+        Pose2d startPose = new Pose2d();
+        drive.setPoseEstimate(startPose);
+        left = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(12, 21), 0)
+                .splineTo(new Vector2d(29, 24), 0)
+                .build();
+        right = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(12, -21), 0)
+                .splineTo(new Vector2d(29, -24), 0)
+                .build();
+        center = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(29, 0), 0)
+                .build();
         robot.camera.requestStart();
         robot.grabber.close();
     }
@@ -38,10 +56,11 @@ public class Park extends BaseOpMode {
         parkingPosition = robot.camera.getParkingPosition();
 
         if (parkingPosition == Camera.ParkingPosition.LEFT) {
-            robot.encoderMecanum.strafeLeft(24);
+            drive.followTrajectory(left);
         } else if (parkingPosition == Camera.ParkingPosition.RIGHT) {
-            robot.encoderMecanum.strafeRight(24);
+            drive.followTrajectory(right);
+        } else {
+            drive.followTrajectory(center);
         }
-        robot.encoderMecanum.driveForward(29);
     }
 }

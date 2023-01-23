eb141dc3d66e733b185bbc8532fcb1d733a7ab49
SofiaAugusta <sasutter518@gmail.com>
2023-01-23 17:41:36 -0600

    Theoretically, left side auto should work. I just used opposite the y values and angles. Last commit was supposed to clean up the right side auto code by removing some comments which I forgot to remove, but those changes are in this one instead.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/NotDone/Left_Deliver1Close_SensePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/NotDone/Left_Deliver1Close_SensePark.java
@@ -11,8 +11,9 @@ import org.firstinspires.ftc.teamcode.Bots.PostBot;
 import org.firstinspires.ftc.teamcode.Components.Camera;
 import org.firstinspires.ftc.teamcode.RoadRunner.drive.RRMecanum;
 
+
 @Autonomous
-public class Right_Deliver1Close_SensePark extends BaseOpMode {
+public class Left_Deliver1Close_SensePark extends BaseOpMode {
     public PostBot robot;
     public RRMecanum drive;
     public Trajectory toPole, forward, backward, left, right, center;
@@ -38,24 +39,24 @@ public class Right_Deliver1Close_SensePark extends BaseOpMode {
         Pose2d startPose = new Pose2d();
         drive.setPoseEstimate(startPose);
         toPole = drive.trajectoryBuilder(startPose, 20,3)
-                .splineTo(new Vector2d(12, 20), 0)
-                .splineTo(new Vector2d(29, 24), 0)
-                .splineToConstantHeading(new Vector2d(29, 32), 0)
-                .splineToConstantHeading(new Vector2d(31, 36.5), 0)
+                .splineTo(new Vector2d(12, -20), 0)
+                .splineTo(new Vector2d(29, -24), 0)
+                .splineToConstantHeading(new Vector2d(29, -32), 0)
+                .splineToConstantHeading(new Vector2d(31, -36.5), 0)
                 .build();
         forward = drive.trajectoryBuilder(toPole.end(),20,3)
-                .splineTo(new Vector2d(33.5, 36.5),0)
+                .splineTo(new Vector2d(33.5, -36.5),0)
                 .build();
         backward = drive.trajectoryBuilder(forward.end())
                 .back(2)
                 .build();
-        left = drive.trajectoryBuilder(backward.end().plus(new Pose2d(0, 0, Math.toRadians(-90))),30,3)
-                .splineTo(new Vector2d(33,27), Math.toRadians(-90))
+        left = drive.trajectoryBuilder(backward.end().plus(new Pose2d(0, 0, Math.toRadians(90))),30,3)
+                .splineTo(new Vector2d(33,-27), Math.toRadians(-90))
                 .build();
-        right = drive.trajectoryBuilder(backward.end().plus(new Pose2d(0, 0, Math.toRadians(-90))),30,3)
-                .splineTo(new Vector2d(33,-24), Math.toRadians(-90))
+        right = drive.trajectoryBuilder(backward.end().plus(new Pose2d(0, 0, Math.toRadians(90))),30,3)
+                .splineTo(new Vector2d(33,24), Math.toRadians(-90))
                 .build();
-        center = drive.trajectoryBuilder(backward.end().plus(new Pose2d(0, 0, Math.toRadians(-90))),30,3)
+        center = drive.trajectoryBuilder(backward.end().plus(new Pose2d(0, 0, Math.toRadians(90))),30,3)
                 .splineTo(new Vector2d(33,0), Math.toRadians(-90))
                 .build();
         robot.camera.requestStart();
@@ -76,7 +77,7 @@ public class Right_Deliver1Close_SensePark extends BaseOpMode {
         sleep(2000);
         drive.followTrajectory(backward);
         robot.arm.toZero();
-        drive.turn(Math.toRadians(-90));
+        drive.turn(Math.toRadians(90));
         if (parkingPosition == Camera.ParkingPosition.LEFT) {
             drive.followTrajectory(left);
         } else if (parkingPosition == Camera.ParkingPosition.RIGHT) {
@@ -85,25 +86,3 @@ public class Right_Deliver1Close_SensePark extends BaseOpMode {
             drive.followTrajectory(center);
         }
     }
-
-//    @Override
-//    public void onUpdate() throws InterruptedException {
-//        switch (state) {
-//            case 0:
-//                if (!drive.isBusy()) {
-//                    drive.followTrajectoryAsync(toPole);
-//                    state++;
-//                }
-//                break;
-//            case 1:
-//                state++;
-//                robot.arm.toHigh();
-//                break;
-//            case 2:
-//                break;
-//            case 100:
-//                break;
-//        }
-//        drive.update();
-//    }
-}

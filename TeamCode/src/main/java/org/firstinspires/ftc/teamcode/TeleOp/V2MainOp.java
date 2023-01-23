237ae991a7db252514d2c1aebec200890dd82fb8
SofiaAugusta <sasutter518@gmail.com>
2023-01-23 16:22:02 -0600

    Created an alternate controller config option for arm controls. (called V2MainOp) Also removed three unimportant lines of code from right side auto that were commented out. This is just for organization purposes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/V2MainOp.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Base.BaseOpMode;
+import org.firstinspires.ftc.teamcode.Base.Robot;
+import org.firstinspires.ftc.teamcode.Bots.PostBot;
+
+@TeleOp
+public class V2MainOp extends BaseOpMode {
+    public PostBot robot;
+    public double x, y, rot, speed, xModifier, yModifier;
+    public boolean slowmode;
+
+    @Override
+    protected Robot setRobot() {
+        this.robot = new PostBot();
+        return this.robot;
+    }
+
+    @Override
+    protected boolean setTeleOp() {
+        return true;
+    }
+
+    @Override
+    public void onInit() throws InterruptedException {
+        robot.grabber.open();
+//        robot.camera.requestStart();
+    }
+
+    @Override
+    public void onStart() throws InterruptedException {
+//        gamepadListener1.y.onPress = () -> {
+//            robot.grabber.close();
+//            robot.arm.toHigh();
+////            driveForward();
+//        };
+//        gamepadListener1.a.onPress = () -> {
+//            robot.grabber.open();
+//            robot.arm.toZero();
+////            driveBackward();
+//        };
+
+        gamepadListener1.start.onRelease = () -> {
+            slowmode = !slowmode;
+        };
+        gamepadListener2.lb.onPress = () -> {
+            robot.grabber.open();
+        };
+        gamepadListener2.rb.onPress = () -> {
+            robot.grabber.close();
+        };
+        gamepadListener2.dd.onPress = () -> {
+            robot.grabber.open();
+            robot.arm.toZero();
+        };
+        gamepadListener2.dr.onPress = () -> {
+            robot.arm.toPickup();
+        };
+        gamepadListener2.du.onPress = () -> {
+            robot.arm.toSideStack();
+        };
+        gamepadListener2.x.onPress = () -> {
+            robot.arm.toGround();
+        };
+        gamepadListener2.a.onPress = () -> {
+            robot.arm.toLow();
+        };
+        gamepadListener2.b.onPress = () -> {
+            robot.arm.toMedium();
+        };
+        gamepadListener2.y.onPress = () -> {
+            robot.arm.toHigh();
+        };
+
+//        gamepadListener2.dr.onPress = () -> {
+//            robot.arm.toZero();
+//            sleep(100);
+//            robot.grabber.close();
+//            robot.arm.toLow();
+//        };
+//
+//        gamepadListener2.du.onPress = () -> {
+//            robot.arm.toZero();
+//            sleep(100);
+//            robot.grabber.close();
+//            robot.arm.toMedium();
+//        };
+    }
+
+    @Override
+    public void onUpdate() throws InterruptedException {
+        speed = (gamepad1.left_bumper ? 0.25 : (gamepad1.right_bumper || slowmode ? 0.5 : 1)) * (gamepad1.left_stick_button ? 1 : 0.65);
+        x = gamepad1.left_stick_x;
+        y = - gamepad1.left_stick_y;
+        rot = gamepad1.right_stick_x;
+
+        if (gamepad1.dpad_up) {
+            y = 1;
+        } else if (gamepad1.dpad_down) {
+            y = - 1;
+        }
+
+        if (gamepad1.dpad_right) {
+            x = 1;
+        } else if (gamepad1.dpad_left) {
+            x = - 1;
+        }
+
+        if (gamepad2.right_trigger > 0 || gamepad2.left_trigger > 0) {
+            if (robot.arm.getCurrentPosition() < robot.arm.LOWER_BOUND) {
+                robot.arm.move(robot.arm.LOWER_BOUND + (int) (robot.arm.PULSES_PER_REVOLUTION * 0.014));
+            } else if (robot.arm.getCurrentPosition() > robot.arm.UPPER_BOUND) {
+                robot.arm.move(robot.arm.UPPER_BOUND - (int) (robot.arm.PULSES_PER_REVOLUTION * 0.014));
+            } else {
+                robot.arm.move((int) ((gamepad2.right_trigger - gamepad2.left_trigger) * robot.arm.PULSES_PER_REVOLUTION * 0.5) + robot.arm.getCurrentPosition(), gamepad2.right_trigger - gamepad2.left_trigger);
+            }
+        }
+
+        robot.mecanum.drive(x * speed, y * speed, rot * speed);
+        telemetry.addData("Raw", robot.arm.getTelemetry());
+        telemetry.update();
+    }
+}
